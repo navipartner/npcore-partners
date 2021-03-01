afc375b6b8976932c4ca9388dc9b4cc18df3cc73
Mikkel Mansa Vilhelmsen <mmv@navipartner.dk>
2021-03-01 17:44:04 +0000

    Merged PR 4710: Better project defaults
    Added workspace config,
    Added workspace recommended extensions
    Moved dependency download powershell script into a vscode task (can be run via ctrl+shift+p -> Run Task).
    
    Related work items: #3138

---
+++ Application/.scripts/download_compile_dependencies.ps1
@@ -0,0 +1,11 @@
+Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-artifacts-credprovider.ps1) } -AddNetfx"
+
+if (-not [System.IO.File]::Exists('.\.netpackages\nuget.exe')) {
+  if (-not [System.IO.Directory]::Exists('.\.netpackages')) {
+    [System.IO.Directory]::CreateDirectory('.\.netpackages')
+  }
+  Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile '.\.netpackages\nuget.exe'
+}
+    
+.\.netpackages\nuget.exe install NaviPartner.NPRetail.AddIns.BC -Source "https://api.nuget.org/v3/index.json;https://pkgs.dev.azure.com/navipartner/_packaging/NprAddIns/nuget/v3/index.json" -OutputDirectory .\.netpackages
+.\.netpackages\nuget.exe install NaviPartner.NPRetail.AddIns.MS.BC -Source https://pkgs.dev.azure.com/navipartner/_packaging/NprAddIns/nuget/v3/index.json -OutputDirectory .\.netpackages
