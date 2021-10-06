06118cf3987708dbeab2d7da12f807e7164d343b
Mikkel Mansa Vilhelmsen <mmv@navipartner.dk>
2021-10-06 07:19:38 +0000

    Merged PR 9186: Documentation publish pipeline adjustments
    Related work items: #5032

--- Documentation/.tools/deploy.ps1
+++ Documentation/.tools/deploy.ps1
@@ -14,6 +14,12 @@ param (
 
 $ErrorActionPreference = 'Stop';
 
+Write-Host "workspaceFolder:" $workspaceFolder
+Write-Host "docfxUrl:" $docfxUrl
+Write-Host "AzureBlobStorageUrl:" $AzureBlobStorageUrl
+Write-Host "AzureCopyUrl:" $AzureCopyUrl
+Write-Host "------------"
+
 Write-Host "Cleaning and creating docfx folders"
 
 $downloadFolder = ($workspaceFolder + "/Documentation/.tools/docfx/download")
@@ -35,8 +41,11 @@ if (-Not ($LastExitCode -eq 0)) {
     throw "docfx returned one or more errors"
 }
 
-Write-Host "Downloading AzCopy"
+Write-Host "Copying API library and openapi files into _site folder"
+Copy-Item -Path ($workspaceFolder + "/Documentation/api/") -Destination ($buildFolder + "/api/") -Recurse
+Copy-Item ($workspaceFolder + "/Documentation/.tools/rapidocs/api.html") -Destination ($buildFolder + "/api/")
 
+Write-Host "Downloading AzCopy"
 $azCopyPath = ($workspaceFolder + "/Documentation/.tools/azcopy")
 Remove-Item $azCopyPath -Recurse -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path $azCopyPath
@@ -46,6 +55,10 @@ $azCopyPath += $AzureCopyUrl.Substring($AzureCopyUrl.LastIndexOf("/"))
 $azCopyPath = $azCopyPath.Substring(0, $azCopyPath.Length-4) #Strip away ".zip"
 
 Write-Host "Uploading _site folder via AzCopy"
+Write-Host "Azure Blob Storage URL:" $AzureBlobStorageUrl
+Write-Host "Azure Blob SAS defined?:" $(If ($AzureBlobStorageSAS.Length -gt 0) {"true"} Else {"false"})
+Write-Host "AzCopy path" $azCopyPath
+Write-Host "Build folder to sync" $buildFolder
 
 & ($azCopyPath + "/azcopy.exe") "sync" $buildFolder ($AzureBlobStorageUrl+"?"+$AzureBlobStorageSAS) "--delete-destination=true"
 
@@ -53,4 +66,4 @@ if (-Not ($LastExitCode -eq 0)) {
     throw "azcopy returned one or more errors"
 }
 
-Write-Host "Documentation website updated: " + $AzureBlobStorageUrl+"/index.html";
+Write-Host "Documentation website successfully updated"
