a5b3c4434f6f264ad3ab2268fd76130e0a682e40
Mikkel Mansa Vilhelmsen <mmv@navipartner.dk>
2021-10-06 11:37:51 +0000

    Merged PR 9213: Flipped paths to windows-style to be compatible with azure devops agent
    Flipped paths to windows-style to be compatible with azure devops agent
    
    Related work items: #5032

--- Documentation/.tools/deploy.ps1
+++ Documentation/.tools/deploy.ps1
@@ -22,35 +22,35 @@ Write-Host "------------"
 
 Write-Host "Cleaning and creating docfx folders"
 
-$downloadFolder = ($workspaceFolder + "/Documentation/.tools/docfx/download")
-$buildFolder = ($workspaceFolder + "/Documentation/.tools/docfx/_site")
+$downloadFolder = ($workspaceFolder + "\Documentation\.tools\docfx\download")
+$buildFolder = ($workspaceFolder + "\Documentation\.tools\docfx\_site")
 Remove-Item $downloadFolder -Recurse -ErrorAction Ignore
 Remove-Item $buildFolder -Recurse -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path $downloadFolder
 
 Write-Host "Downloading docfx"
 
-Invoke-WebRequest -OutFile ($downloadFolder+"/docfx.zip") $docfxUrl
-Expand-Archive -Path ($downloadFolder+"/docfx.zip") -DestinationPath $downloadFolder
+Invoke-WebRequest -OutFile ($downloadFolder+"\docfx.zip") $docfxUrl
+Expand-Archive -Path ($downloadFolder+"\docfx.zip") -DestinationPath $downloadFolder
 
 Write-Host "Executing docfx"
 
-& ($downloadFolder + "/docfx.exe") ($workspaceFolder + "/Documentation/.tools/docfx/docfx.json") "--warningsAsErrors"
+& ($downloadFolder + "\docfx.exe") ($workspaceFolder + "\Documentation\.tools\docfx\docfx.json") "--warningsAsErrors"
 
 if (-Not ($LastExitCode -eq 0)) {
     throw "docfx returned one or more errors"
 }
 
 Write-Host "Copying API library and openapi files into _site folder"
-Copy-Item -Path ($workspaceFolder + "/Documentation/api/") -Destination ($buildFolder + "/api/") -Recurse
-Copy-Item ($workspaceFolder + "/Documentation/.tools/rapidocs/api.html") -Destination ($buildFolder + "/api/")
+Copy-Item -Path ($workspaceFolder + "\Documentation\api\") -Destination ($buildFolder + "\api\") -Recurse
+Copy-Item ($workspaceFolder + "\Documentation\.tools\rapidocs\api.html") -Destination ($buildFolder + "\api\")
 
 Write-Host "Downloading AzCopy"
-$azCopyPath = ($workspaceFolder + "/Documentation/.tools/azcopy")
+$azCopyPath = ($workspaceFolder + "\Documentation\.tools\azcopy")
 Remove-Item $azCopyPath -Recurse -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path $azCopyPath
-Invoke-WebRequest -OutFile ($azCopyPath+"/azcopy.zip") $AzureCopyUrl
-Expand-Archive -Path ($azCopyPath+"/azcopy.zip") -DestinationPath $azCopyPath
+Invoke-WebRequest -OutFile ($azCopyPath+"\azcopy.zip") $AzureCopyUrl
+Expand-Archive -Path ($azCopyPath+"\azcopy.zip") -DestinationPath $azCopyPath
 $azCopyPath += $AzureCopyUrl.Substring($AzureCopyUrl.LastIndexOf("/"))
 $azCopyPath = $azCopyPath.Substring(0, $azCopyPath.Length-4) #Strip away ".zip"
 
@@ -60,7 +60,7 @@ Write-Host "Azure Blob SAS defined?:" $(If ($AzureBlobStorageSAS.Length -gt 0) {
 Write-Host "AzCopy path" $azCopyPath
 Write-Host "Build folder to sync" $buildFolder
 
-& ($azCopyPath + "/azcopy.exe") "sync" $buildFolder ($AzureBlobStorageUrl+"?"+$AzureBlobStorageSAS) "--delete-destination=true"
+& ($azCopyPath + "\azcopy.exe") "sync" $buildFolder ($AzureBlobStorageUrl+"?"+$AzureBlobStorageSAS) "--delete-destination=true"
 
 if (-Not ($LastExitCode -eq 0)) {
     throw "azcopy returned one or more errors"
