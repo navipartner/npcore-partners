b5f2513ccd23b717916cbfab6ef5e571181da59d
Mikkel Mansa Vilhelmsen <mmv@navipartner.dk>
2022-09-15 18:05:29 +0000

    Merged PR 15050: Adjusted external terminal integration to match latest EFT module changes
    Adjusted external terminal integration to match latest EFT module changes
    
    Related work items: #6465

--- Application/src/POS Payment/EFT/Integrations/External Terminal/POSActionExtTerminal.js
+++ Application/src/POS Payment/EFT/Integrations/External Terminal/POSActionExtTerminal.js
@@ -1,30 +1,30 @@
 let main = async ({ context, workflow, popup, captions }) => {
-  let result = { "success": false, "endSale": false };
+  let result = { "success": false, "tryEndSale": false };
 
-  if (context.hwcRequest.AmountIn > 0) {
-    if (context.hwcRequest.PromptCardDigits) {
-      context.hwcRequest.CardDigits = await popup.intpad({ caption: captions.PromptCardDigits });
-      if (context.hwcRequest.CardDigits === null || context.hwcRequest.CardDigits === 0) return (result);
+  if (context.request.AmountIn > 0) {
+    if (context.request.PromptCardDigits) {
+      context.request.CardDigits = await popup.intpad({ caption: captions.PromptCardDigits });
+      if (context.request.CardDigits === null || context.request.CardDigits === 0) return (result);
     };
 
-    if (context.hwcRequest.PromptCardHolder) {
-      context.hwcRequest.CardHolder = await popup.input({ caption: captions.PromptCardHolder });
-      if (context.hwcRequest.CardHolder === null) return (result);
+    if (context.request.PromptCardHolder) {
+      context.request.CardHolder = await popup.input({ caption: captions.PromptCardHolder });
+      if (context.request.CardHolder === null) return (result);
     };
 
-    if (context.hwcRequest.PromptApprovalCode) {
-      confirmAns = await popup.confirm(captions.PromptConfirmation + context.hwcRequest.AmountIn + "?");
+    if (context.request.PromptApprovalCode) {
+      confirmAns = await popup.confirm(captions.PromptConfirmation + context.request.AmountIn + "?");
       if (!confirmAns) return (result);
-      context.hwcRequest.ApprovalCode = await popup.input({ caption: captions.PromptApprovalCode });
-      if (context.hwcRequest.ApprovalCode === "") {
+      context.request.ApprovalCode = await popup.input({ caption: captions.PromptApprovalCode });
+      if (context.request.ApprovalCode === "") {
         popup.error(captions.InvalidApprovalCode);
         return (result);
       };
-      if (context.hwcRequest.ApprovalCode === null) return (result);
+      if (context.request.ApprovalCode === null) return (result);
     };
   };
   const { success, endSale } = await workflow.respond("FinalizeRequest");
   result.success = success;
-  result.endSale = endSale;
+  result.tryEndSale = endSale;
   return (result);
 }
