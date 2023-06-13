751b0819951fb18343967398efe6192454e6198f
Radoslav Yordanov <rayo@navipartner.com>
2023-06-13 09:25:25 +0000

    Merged PR 19699: Case: 576663 Cannot delete cash payments
    Case: 576663 Cannot delete cash payments
    
    Check POS View when deleting lines on the POS.
    
    There was a bug where you were not able to delete the payment lines if there were no sales lines on the POS.
    The problem was caused because we were not checking the current POS View and we were always checking if the sales line were valid.
    Now if you're in the POS Payment View the system is going to delete the payment lines.
    
    Related work items: #12731

--- Application/src/POS Action (with Misc actions)/Delete Line/POSActionDeleteLine.js
+++ Application/src/POS Action (with Misc actions)/Delete Line/POSActionDeleteLine.js
@@ -1,14 +1,25 @@
-let main = async ({workflow,parameters, captions}) => {
-    let saleLines = runtime.getData("BUILTIN_SALELINE");
-    if ((!saleLines.length) || (saleLines._invalid)) {
+let main = async ({ workflow, parameters, captions }) => {
+    debugger;
+    var getCurrentViewTypeResponse = await workflow.respond('GetCurrentViewType');
+    var currLine;
+
+    switch (getCurrentViewTypeResponse.viewType) {
+        case "Payment":
+            currLine = runtime.getData("BUILTIN_PAYMENTLINE");
+            break;
+        default:
+            currLine = runtime.getData("BUILTIN_SALELINE");
+    }
+
+    if ((!currLine.length) || (currLine._invalid)) {
         await popup.error(captions.notallowed);
         return;
     };
 
     if (parameters.ConfirmDialog) {
-        if (!await popup.confirm({ title: captions.title, caption: captions.Prompt.substitute(saleLines._current[10]) })) {
+        if (!await popup.confirm({ title: captions.title, caption: captions.Prompt.substitute(currLine._current[10]) })) {
             return;
         };
     };
-    workflow.respond();
+    workflow.respond('DeletePosLine');
 };
