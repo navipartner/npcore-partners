5322acbbfdbd6e11d58be2b1e497845a15e89b48
Nikolina Duvnjak <nidu@navipartner.com>
2023-06-23 16:23:45 +0000

    Merged PR 19944: Open the Cash Drawer
    Open the Cash Drawer after question for Confirm Bin
    
    Related work items: #12730

--- Application/src/POS Action (with Misc actions)/StartPOS/POSActionStartPOS.js
+++ Application/src/POS Action (with Misc actions)/StartPOS/POSActionStartPOS.js
@@ -2,17 +2,13 @@ let main = async ({workflow, context, captions, popup}) => {
     debugger;
     const{ConfirmBin, BinContents, BalancingIsNotAllowed, EoDActionCode} = await workflow.respond("OnBeforeStartPOS");
     if (ConfirmBin) {
+        await workflow.respond('OpenCashDrawer');
         context.confirm = await popup.confirm({title: captions.bincontenttitle, caption: BinContents});
         if (!context.confirm) {
             if (BalancingIsNotAllowed) {
                 popup.error(captions.BalancingIsNotAllowedError);
             } else {
-                let confirmBalancing = await popup.confirm({title: captions.binbalancetitle, caption: captions.balancenow});
-                if (confirmBalancing) {
-                    workflow.run(EoDActionCode, {parameters: {Type: 1}})
-                } else {
-                    popup.message({ title: captions.notconfirmedbintitle, caption: captions.NotConfirmedBin, });
-                }
+                workflow.run(EoDActionCode, {parameters: {Type: 1}})
             }
         } else {
             await workflow.respond("ConfirmBin");
