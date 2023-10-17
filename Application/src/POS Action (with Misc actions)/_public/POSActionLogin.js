bf448cafccca1746faf1704ac0f3f602a3a7ae4a
Radoslav Yordanov <rayo@navipartner.com>
2023-10-17 12:12:31 +0000

    Merged PR 21696: Skip Select Member and Select Customer when pos not balanced
    If the 'Select Customer' and the 'Select Member' after login actions are enabled, they are executed even if the pos is not balanced. The current pr aims to modify the behavior of the OnAfterLogin actions so that they are executed only of the user actually logs into the pos.

--- Application/src/POS Action (with Misc actions)/_public/POSActionLogin.js
+++ Application/src/POS Action (with Misc actions)/_public/POSActionLogin.js
@@ -1,27 +1,32 @@
-let main = async ({ workflow, context}) => {
+let main = async ({ workflow, context }) => {
     debugger;
     let result = await workflow.respond("prepareWorkflow");
     const preWorkflows = result.preWorkflows;
 
-    if (preWorkflows) {
-        for (const preWorkflow of Object.entries(preWorkflows)) {
-            let [preWorkflowName, preWorkflowParameters] = preWorkflow;
-            if (preWorkflowName) {
-                let {mainParameters, customParameters} = preWorkflowParameters;
-                await workflow.run(preWorkflowName, {context: { customParameters: customParameters }, parameters: mainParameters});
-            };
-        };
-    };
-
     if (result.workflowName == "") {
+        await processPreWorkflows(preWorkflows);
         return;
     }
 
     if (result.workflowName == 'START_POS') {
-        await workflow.run(result.workflowName);
+        const { posStarted } = await workflow.run(result.workflowName);
+        if (posStarted) {
+            await processPreWorkflows(preWorkflows);
+        };
     }
-    else
-    {
+    else {
         await workflow.run(result.workflowName, { parameters: result.parameters })
     }
 };
+
+async function processPreWorkflows(preWorkflows) {
+    if (preWorkflows) {
+        for (const preWorkflow of Object.entries(preWorkflows)) {
+            let [preWorkflowName, preWorkflowParameters] = preWorkflow;
+            if (preWorkflowName) {
+                let { mainParameters, customParameters } = preWorkflowParameters;
+                await workflow.run(preWorkflowName, { context: { customParameters: customParameters }, parameters: mainParameters });
+            };
+        };
+    };
+}
